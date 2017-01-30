098030de72b2e40c6d6eb0f99f1a7583d79c2992
JL <jeanlouis02300@gmail.com>
2017-01-30 09:46:37 +0100

    indication pour le fichier config

--- config.php
+++ config.php
@@ -3,6 +3,7 @@ date_default_timezone_set('Europe/Paris');
 $urlSite =  $_SERVER['HTTP_HOST'];
 try
 {
+  // ici vos paarametre de connexion a votre base de donnée
   $base = new PDO('mysql:host=localhost;dbname=prsm;charset=utf8', 'root', 'root');
 }
 catch (Exception $e)
