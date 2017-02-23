acd28be5142b884a21cc1d1333e1bc3a14b81db0
JL <jeanlouis02300@gmail.com>
2017-02-23 11:30:03 +0100

    full PDO

--- charger.php
+++ charger.php
@@ -1,25 +1,19 @@
 <?php
+session_start();
 include_once 'config.php';
-// ...
-// on se connecte à notre base de données
-usleep(5000000);
-if(!empty($_GET['id'])){ // on vérifie que l'id est bien présent et pas vide
-
-    $id = (int) $_GET['id']; // on s'assure que c'est un nombre entier
-
-    include_once 'config.php';
-    date_default_timezone_set('Europe/Paris');
+if(!empty($_GET['id'])){
+    $id = htmlspecialchars($_GET['id'], ENT_QUOTES);
     $datelast10min = (strtotime(date("d-m-Y H:i:s"))) - 600;
-    $requete_donnees_messages = $base->query("SELECT * FROM prsm_msgs WHERE id > $id AND datemsg > $datelast10min");
-    $requete_donnees_messages->setFetchMode(PDO::FETCH_OBJ);
-    $message = null;
-    while( $donnees_messages = $requete_donnees_messages->fetch() )
-    {
-      $message .= "<li id='".$donnees_messages->id."' class='".$donnees_messages->userCouleur." ".$donnees_messages->userVal." white-text dialogue'>";
-      $message .= $donnees_messages->contenu;
-      $message .= "<div class='right-align grey-text text-lighten-2 signature'>".$donnees_messages->userNom."</div>";
-      $message .= "</li>";
+    $requete_donnees_messages = $base->prepare("SELECT id, userCouleur, userVal, contenu, userNom FROM prsm_msgs WHERE id > $id AND datemsg > $datelast10min");
+    if ($requete_donnees_messages->execute()) {
+      $message = null;
+      while( $donnees_messages = $requete_donnees_messages->fetch() ){
+        $message .= "<li id='".$donnees_messages['id']."' class='".$donnees_messages['userCouleur']." ".$donnees_messages['userVal']." white-text dialogue'>";
+        $message .= $donnees_messages['contenu'];
+        $message .= "<div class='right-align grey-text text-lighten-2 signature'>".$donnees_messages['userNom']."</div>";
+        $message .= "</li>";
+      }
+      echo $message;
     }
-    echo $message;
 }
 ?>
--- upmsg.php
+++ upmsg.php
@@ -2,23 +2,35 @@
 include_once 'config.php';
 session_start();
 if (isset($_SESSION['connecte'])){
-    $userIdUnique = $_SESSION["userIdUnique"];
-    $userNom = $_SESSION["userNom"];
-    $userCouleur = $_SESSION["userCouleur"];
-    $userVal = $_SESSION["userVal"];
+    $userIdUnique = htmlspecialchars($_SESSION["userIdUnique"], ENT_QUOTES);
+    $userNom = htmlspecialchars($_SESSION["userNom"], ENT_QUOTES);
+    $userCouleur = htmlspecialchars($_SESSION["userCouleur"], ENT_QUOTES);
+    $userVal = htmlspecialchars($_SESSION["userVal"], ENT_QUOTES);
     $message = htmlspecialchars($_POST["message"], ENT_QUOTES);
-    date_default_timezone_set('Europe/Paris');
     $dateentrée = strtotime(date("d-m-Y H:i:s"));
 
-    $base->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "INSERT INTO prsm_msgs (contenu, userIdUnique, userNom, userCouleur, userVal, datemsg)
-    VALUES ('$message', '$userIdUnique', '$userNom', '$userCouleur', '$userVal', '$dateentrée')";
-    $base->exec($sql);
-
-    $base->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $sql = "UPDATE prsm_users SET
-    lastconnect = '$dateentrée'
-    WHERE idunique = '$userIdUnique'";
-    $base->exec($sql);
+    //Update des messages en bdd
+    $stmt = $base->prepare("INSERT INTO prsm_msgs  (contenu, userIdUnique, userNom, userCouleur, userVal, datemsg) VALUES (?, ?, ?, ?, ?, ?)");
+    $stmt->bindParam(1, $datacontenu);
+    $stmt->bindParam(2, $datauserIdUnique);
+    $stmt->bindParam(3, $datauserNom);
+    $stmt->bindParam(4, $datauserCouleur);
+    $stmt->bindParam(5, $datauserVal);
+    $stmt->bindParam(6, $datadatemsg);
+    // insertion d'une ligne
+    $datacontenu = $message;
+    $datauserIdUnique = $userIdUnique;
+    $datauserNom = $userNom;
+    $datauserCouleur = $userCouleur;
+    $datauserVal = $userVal;
+    $datadatemsg = $dateentrée;
+    $stmt->execute();
+    
+    //Update de la liste des utilisateurs
+    $stmt = $base->prepare("INSERT INTO prsm_users (lastconnect) VALUES (?) WHERE idunique = '$userIdUnique'");
+    $stmt->bindParam(1, $datacontenu);
+    // insertion d'une ligne
+    $datadateentrée = $dateentrée;
+    $stmt->execute();
 }
  ?>
